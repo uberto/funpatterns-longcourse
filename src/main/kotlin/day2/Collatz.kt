3f259c3eff71853d6ef80a0fdf292569f748e9c8
uberto <uberto.gama@gmail.com>
2019-08-02 17:29:51 +0100

    collatz sequence example

--- src/main/kotlin/day2/Collatz.kt
+++ src/main/kotlin/day2/Collatz.kt
@@ -5,3 +5,11 @@ fun Int.collatz(): List<Int> = listOf(this) + if (this % 2 == 0) evenCase(this) 
 
 private fun evenCase(n: Int) = (n / 2).collatz()
 private fun oddCase(n: Int) = if (n == 1) listOf() else (n * 3 + 1).collatz()
+
+
+
+
+var x = 1
+fun collatzMax(): Sequence<Int> = generateSequence {
+    x.collatz().max().also { x += 1 }
+}
