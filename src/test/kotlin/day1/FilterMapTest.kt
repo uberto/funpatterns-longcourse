9b5441100a81a7c1fd628c8eaf9783c6319d282d
uberto <uberto.gama@gmail.com>
2019-08-02 17:02:33 +0100

    filterMap example

---
+++ src/test/kotlin/day1/FilterMapTest.kt
@@ -0,0 +1,21 @@
+package day1
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class FilterMapTest(){
+
+    fun <T: Any, U> List<T>.filterMap(f: (T) -> U?): List<U> = this.map(f).filterNot { it  == null }.map { it!! }
+
+    @Test
+    fun `filterMap transform values and remove the null ones`(){
+
+        val names = listOf( "Frank", "John", "Mary", "Ann", "Fred")
+
+        val res = names.filterMap{ n -> if(n.length == 4 ) n.first() else null }
+
+        assertThat(res).isEqualTo(listOf('J', 'M', 'F'))
+
+    }
+}
