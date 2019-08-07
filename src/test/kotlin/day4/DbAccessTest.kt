144d17c9dd7d30f5358bf3792ec09b4c473c422c
uberto <uberto.gama@gmail.com>
2019-08-07 15:45:01 +0100

    adding flatmap operations test

--- src/test/kotlin/day4/DbAccessTest.kt
+++ src/test/kotlin/day4/DbAccessTest.kt
@@ -11,30 +11,62 @@ class DbAccessTest {
 
     val db = InMemoryDbAccess(mutableListOf(), mutableListOf())
 
+    private fun <E: Error, T: Any> Outcome<E, T>.expectSuccess(): T = this.onFailure { fail(it.msg) }
+
+    fun DbAccess.getUserNameFromOrder(orderId: OrderId): Outcome<DbError, String> = with(this) {
+        orderId.read().flatMap {
+            it.userId.read().map { it.name }
+        }
+    }
+
+    fun DbAccess.duplicateOrderWithSameAmountAndNewUser(orderId: OrderId, userId: UserId): Outcome<DbError, OrderId> =
+        with(this) {
+            orderId.read().flatMap { o ->
+                userId.read().flatMap { u ->
+                    o.copy(id = OrderId(3), userId = u.id).write()
+                }
+
+            }
+        }
+
     private fun preparedDb() =
         db.apply {
-            val bob = User(UserId(1), "Bob").write().onFailure { fail(it.msg) }
+            val bob = User(UserId(1), "Bob").write().expectSuccess()
 
-            val alice = User(UserId(2), "Alice").write().onFailure { fail(it.msg) }
+            val alice = User(UserId(2), "Alice").write().expectSuccess()
 
-            Order(OrderId(1), bob, 123.4).write().onFailure { fail(it.msg) }
+            Order(OrderId(1), bob, 123.4).write().expectSuccess()
 
-            Order(OrderId(2), alice, 234.5).write().onFailure { fail(it.msg) }
+            Order(OrderId(2), alice, 234.5).write().expectSuccess()
         }
 
 
     @Test
-    fun `read user name from order`(){
+    fun `read user name from order`() {
 
-        with(preparedDb()){
+        val userName = preparedDb().getUserNameFromOrder(OrderId(1)).expectSuccess()
 
-            val order = OrderId(1).read()
-            val user = order.flatMap {
-                it.userId.read().map { it.name }
-            }
+        assertThat(userName).isEqualTo("Bob")
 
-            assertThat(user).isEqualTo("Bob")
+    }
+
+    @Test
+    fun `duplicate order with same amount a new user`() {
+
+        with(preparedDb()) {
+            val newId = duplicateOrderWithSameAmountAndNewUser(OrderId(1), UserId(2))
+
+            newId.map {
+                val o = it.read().expectSuccess()
+
+                assertThat( o.id ).isEqualTo(OrderId(3))
+
+                assertThat( o.userId ).isEqualTo(UserId(2))
+                assertThat( o.amount ).isEqualTo(123.4)
+            }.expectSuccess()
+
         }
+
     }
 
 }
