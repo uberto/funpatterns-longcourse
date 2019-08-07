7a3a4250645679ae4cfb8bbc6cac4a76a7ef757a
uberto <uberto.gama@gmail.com>
2019-08-07 10:58:02 +0100

    made collatz tailrec

--- src/main/kotlin/day2/Collatz.kt
+++ src/main/kotlin/day2/Collatz.kt
@@ -1,13 +1,12 @@
 package day2
 
 
-fun Int.collatz(): List<Int> = TODO()
+fun Int.collatz() = collatzR(listOf(), this)
 
-private fun evenCase(n: Int) = (n / 2).collatz()
-private fun oddCase(n: Int) = if (n == 1) listOf() else (n * 3 + 1).collatz()
-
+tailrec fun collatzR(acc: List<Int>, x: Int): List<Int> = TODO()
 
 
-
-var x = 1
-fun collatzMax(): Sequence<Int> = TODO() //return the max value in the collatz seq for the number
+var hiddenX = 1
+fun collatzMax(): Sequence<Int> = generateSequence {
+    TODO()
+}
