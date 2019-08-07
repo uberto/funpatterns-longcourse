eb8906657bf05ba093660c74553377db49ea6177
uberto <uberto.gama@gmail.com>
2019-08-07 11:02:57 +0100

    tidy up

--- src/main/kotlin/day2/Collatz.kt
+++ src/main/kotlin/day2/Collatz.kt
@@ -3,7 +3,11 @@ package day2
 
 fun Int.collatz() = collatzR(listOf(), this)
 
-tailrec fun collatzR(acc: List<Int>, x: Int): List<Int> = TODO()
+tailrec fun collatzR(acc: List<Int>, x: Int): List<Int> = when {
+    x == 1 -> TODO()
+    x % 2 == 0 -> TODO()
+    else -> TODO()
+}
 
 
 var hiddenX = 1
