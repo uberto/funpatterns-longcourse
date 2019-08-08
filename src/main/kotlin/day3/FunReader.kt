185add1888fc679ccedf8767d7436ed735bf3a3d
uberto <uberto.gama@gmail.com>
2019-08-08 13:14:54 +0100

    tidy up

--- src/main/kotlin/day3/FunReader.kt
+++ src/main/kotlin/day3/FunReader.kt
@@ -1,10 +1,12 @@
 package day3
 
+import day1.andThen
 
-data class FunReader<C, A>(val runner: (C) -> A): Reader<C, A> {
+
+data class FunReader<C, A>(val runner: (C) -> A) : Reader<C, A> {
 
     override fun runReader(context: C): A = runner(context)
 
-    override fun local(f: (C) -> C): Reader<C, A> = FunReader { ctx -> runReader(f(ctx)) }
+    override fun local(f: (C) -> C): Reader<C, A> = FunReader(f andThen runner)
 
 }
