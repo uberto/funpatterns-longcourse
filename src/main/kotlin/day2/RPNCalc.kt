3957c334f0b764440c407aad324da9a133943077
uberto <uberto.gama@gmail.com>
2019-08-08 15:39:27 +0100

    rewrote RPNcalc using FunctionalStack

--- src/main/kotlin/day2/RPNCalc.kt
+++ src/main/kotlin/day2/RPNCalc.kt
@@ -1,32 +1,37 @@
 package day2
 
-import java.util.*
-
-object RPNCalc {
-
-    val stack = ArrayDeque<Double>()
-
-    fun calc(cmd: String): Double {
-        val elements = cmd.split(" ")
-
-        return elements.fold(0.0, RPNCalc::rpn)
-    }
-
-    private fun rpn(acc: Double, elem: String): Double = elem
-        .toDoubleOrNull()
-        ?.let { stack.push(it); acc }
-        ?: operation(elem, stack.pop(), stack.pop()).also (stack::push)
-
-    private fun operation(op: String, x: Double, y: Double): Double {
-        val res = when (op) {
-            "+" -> x + y
-            "-" -> x - y
-            "*" -> x * y
-            "/" -> x / y
-            else -> throw Exception( "Unknown operation $op")
-        }
-        return res
-    }
+import day1.FunStack
 
 
+fun calc(cmd: String): Double? {
+    val elements = cmd.split(" ")
+
+    val (_, r) = elements.fold(FunStack(), ::rpn).pop()
+    return r
 }
+
+private fun rpn(stack: FunStack<Double>, elem: String): FunStack<Double> = elem
+    .toDoubleOrNull()
+    ?.let { stack.push(it) }
+    ?: stack.pop().let { (s1, v1) ->
+        s1.pop().let { (s2, v2) ->
+            if (v1 == null)
+                return stack
+            if (v2 == null)
+                return stack
+            s2.push(operation(elem, v1, v2))
+        }
+    }
+
+private fun operation(op: String, x: Double, y: Double): Double {
+    val res = when (op) {
+        "+" -> x + y
+        "-" -> x - y
+        "*" -> x * y
+        "/" -> x / y
+        else -> throw Exception("Unknown operation $op")
+    }
+    return res
+}
+
+
--- src/test/kotlin/day2/RPNCalcTest.kt
+++ src/test/kotlin/day2/RPNCalcTest.kt
@@ -10,7 +10,7 @@ class RPNCalcTest {
     fun `fist operand pop up 2 elements from stack`(){
         val expr = "4 5 +"
 
-        val res = RPNCalc.calc(expr)
+        val res = calc(expr)
 
         assertThat(res).isEqualTo(9.0)
     }
@@ -19,7 +19,7 @@ class RPNCalcTest {
     fun `numbers are pushed on the stack until requested`(){
         val expr = "3 2 1 + +"
 
-        val res = RPNCalc.calc(expr)
+        val res = calc(expr)
 
         assertThat(res).isEqualTo(6.0)
     }
@@ -28,8 +28,17 @@ class RPNCalcTest {
     fun `all operations pop number from stack`(){
         val expr = "5 3 5 4 5 + * / -"
 
-        val res = RPNCalc.calc(expr)
+        val res = calc(expr)
 
         assertThat(res).isEqualTo(10.0)
+    }
+
+    @Test
+    fun `in case of no value return null`(){
+        val expr = "+ * / -"
+
+        val res = calc(expr)
+
+        assertThat(res).isEqualTo(null)
     }
 }
